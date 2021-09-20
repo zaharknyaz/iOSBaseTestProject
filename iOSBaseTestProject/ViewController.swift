24c9bc2cc4c7491a217e85e037d86a7fec478b4d
Zahar Knyazev <knyazzz1989@mail.ru>
2021-09-20 21:07:27 +0400

    Initial Commit

---
+++ iOSBaseTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSBaseTestProject
+//
+//  Created by Захар Князев on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
